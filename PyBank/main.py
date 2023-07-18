288a4eff2f796bd0681cc481e798bcf8b1368130
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-07-18 15:51:49 -0500

    completed assignment

---
+++ PyBank/main.py
@@ -0,0 +1,72 @@
+# import libraries 
+import csv
+import os
+
+# set file path to csv and txt files
+csvpath = os.path.join("resources\\budget_data.csv")
+analysis = os.path.join("analysis","analysis.txt")
+
+
+# set variables
+total_months = 0
+profit_loss = []
+total_amount = 0
+prev_year_list = []
+greatest_increase=["",-1000]
+greatest_decrease = ["", 999999]
+
+# open file
+with open(csvpath) as csvfile:
+     
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    # skip the header row and first row
+    csvheader = next(csvreader)
+    first_row = next(csvreader)
+    prev_year = int(first_row[1])
+    total_months = total_months + 1
+    total_amount = total_amount + int(first_row[1])
+    
+    # loop through to find total months and total amount
+    for row in csvreader:
+       total_months = total_months + 1
+       total_amount = total_amount + int(row[1])
+
+       # Calculating change over a period
+       profit_loss.append(int(row[1]))
+       change=int(row[1])-prev_year
+       prev_year_list.append(change)
+       prev_year=int(row[1])
+
+       # Calculating greatest increase and greatest decrease
+       if change > greatest_increase[1]:
+           greatest_increase[0]=row[0]
+           greatest_increase[1]=change
+       if change < greatest_decrease[1]:
+           greatest_decrease[0]=row[0]
+           greatest_decrease[1]=change
+
+    # Calculate average change
+    avg = sum(prev_year_list)/len(prev_year_list)
+    
+    # round the average
+    avg = round(avg,2)
+       
+
+# write the result to a text file.
+with open(analysis,"a") as txtfile:
+    txtfile.write(f"Financial Analysis\n"
+                  f"------------------------\n"
+                  f"Total months : {(total_months)} \n" 
+                  f"Total : ${total_amount} \n"
+                  f"Average Change : ${avg} \n"
+                  f"Greatest Increase in Porfits: {greatest_increase[0]}, (${greatest_increase[1]}) \n"
+                  f"Greatest Decrease in Profits: {greatest_decrease[0]}, (${greatest_decrease[1]}) "
+                  )
+    
+    #print results 
+    print(f"Total months :{total_months} \n")
+    print(f"Total: ${total_amount} \n")
+    print(f"Average change : ${avg} \n")
+    print(f"Greatest Increase in Porfits: {greatest_increase[0]}, (${greatest_increase[1]}) \n")
+    print(f"Greatest Decrease in Profits: {greatest_decrease[0]}, (${greatest_decrease[1]})")
---
+++ PyPoll/main.py
@@ -0,0 +1,78 @@
+# import libraries
+import os
+import csv
+
+# set file path to csv and text files
+csvpath = os.path.join("resources\election_data.csv")
+analysis = os.path.join("analysis","analysis.txt")
+
+# set variables
+total_votes = 0
+candidates = []
+candidate_vote = {}
+winner_votes = 0
+winner = " "
+
+# open file
+with open (csvpath) as csvfile:
+    
+    # set reader and header
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    csvheader = next(csvreader)
+
+    # loop through to count total votes per candidate
+    for row in csvreader:
+        total_votes = total_votes + 1
+        candidate = row[2]
+
+        # if statement to find candidate names
+        # add them to a dictionary with candidate name as keys and votes as values
+        if candidate not in candidates:
+            candidates.append(candidate)
+            candidate_vote[candidate]=0
+        candidate_vote[candidate]=candidate_vote[candidate]+1
+
+# writing in output text file        
+with open(analysis, "a") as txtfile:
+    txtfile.write( f"Election Results \n"
+                  
+                  f"---------------------\n"
+                  f"Total votes: {total_votes} \n"
+
+                  f"---------------------\n"
+                  )    
+    # loop to find vote percentage from candidate_vote dictionary 
+    for candidate in candidate_vote:
+        votes = candidate_vote[candidate]
+        vote_percent = votes / total_votes * 100
+
+        #round to 3 decimal spaces.
+        vote_percent = round(vote_percent,3)
+
+
+        # find most number of votes and candidate name
+        # write the percent of votes per candidate in the loop
+        if (votes > winner_votes):
+            winner_votes = votes
+            winner = candidate
+
+
+        txtfile.write(f"{candidate} : {vote_percent}% ({(votes)})  \n")
+    
+# Write the winner in output text file
+with open(analysis, "a") as txtfile:
+    txtfile.write(f"---------------------\n"
+
+                  f"Winner : {winner} \n"
+                  f"----------------------\n"                  
+                  )
+    
+    # print results:
+
+    print(f"Total votes : {total_votes} \n")
+
+    for candidate in candidate_vote:
+        print(f"{candidate} : {vote_percent}% ({(votes)})  \n")
+
+
+    print(f"Winner : {winner}")
